b402f25d9147e01424918268e83a114c351cca6a
Mykola Shumakov <mshumako@cisco.com>
2019-04-02 16:32:25 +0300

    render form was added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :tittle, presence: true,
-                     length: { minimum: 5 }
+  validates :title, presence: true,
+                    length: {minimum: 5}
 end
